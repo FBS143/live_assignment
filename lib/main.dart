27f8eb8a46757dffe459dc5d3d6146bfb44ce6d1
FBS143 <srijon143123@gmail.com>
2024-11-15 22:27:55 +0600

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,59 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: bloodDonationScreen(),
+    );
+  }
+}
+
+class bloodDonationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        centerTitle: true,
+        title: Text(
+          'Need Blood',
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.grey[700],
+              child: Icon(
+                Icons.bloodtype,
+                color: Colors.redAccent,
+                size: 80,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Donate Blood',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
